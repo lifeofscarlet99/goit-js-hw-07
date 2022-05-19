3fb80f2f336b60428b464077a32608ecf433464c
Anastasia Slezko <slezkoanastasiia@gmail.com>
2022-05-19 17:05:03 +0300

    3 sample
    3 sample

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,9 +9,9 @@ const markupElements = createImageMarkup(galleryItems);
 function createImageMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
+      return `<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>
+</a></li>
     `;
     })
     .join("");
